c4dba85e716f77e09c7572eee61fd787f0fb6b56
Brandon Jenkins <90408885+imbrandonj@users.noreply.github.com>
2022-10-26 22:27:36 -0500

    added problem set 5

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // global constants used as problem IDs and answers
-const capAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";  // problem set 1
+const upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";  // problem set 1
 const lowerAlpha = "abcdefghijklmnopqrstuvwxyz";  // problem set 2
 const countEmoji = ["L2", "L4", "L8", "B3", "B5", "B7", "B9"];  // problem set 3
+const spell = ["cat", "dog", "cow", "pig", "snake", "bear", "lion", "tiger", "frog", "ear", "nose", "mouth", "bee", "ant" ] // problem set 5
 
 // global variables to be used between functions
 let answer;  // user answer
@@ -11,8 +12,9 @@ let totalCorrect = 0;  // total correct answers tally
 let set2 = 0; // correct tally of problem set 2
 let set3 = 0; // correct tally of problem set 3
 let set4 = 0;  // correct tally of problem set 4
+let set5 = 0; // correct tally of problem set 5
 
-loadProblem();  // sets first problem on page load
+window.onload = loadProblem();  // sets first problem on page load
 
 // functions and events:
 
@@ -23,20 +25,24 @@ function loadProblem() {
     while ( previousProblem === problemSelect ) {  // this loop prevents a duplicate
 
         if ( totalCorrect < 20 )
-            // problem set 1 using capAlpha var
-            problemSelect = capAlpha.charAt(Math.floor(Math.random() * capAlpha.length));  // selects random alpha char
+            // problem set 1 using upperAlpha const
+            problemSelect = upperAlpha.charAt(Math.floor(Math.random() * upperAlpha.length));  // selects random alpha char
 
         else if ( totalCorrect < 40 ) 
-            // problem set 2 using lowerAlpha var
+            // problem set 2 using lowerAlpha const
             problemSelect = lowerAlpha.charAt(Math.floor(Math.random() * lowerAlpha.length));  // selects random lower char
 
         else if ( totalCorrect < 60 ) 
             // problem set 3 using countEmoji array
             problemSelect = countEmoji[Math.floor(Math.random() * countEmoji.length)];  // selects random count emoji
 
-        else 
+        else if ( totalCorrect < 80 )
             // problem set 4 using numbers
             problemSelect = String(Math.floor(Math.random() * ( 11 - 2 ) + 2));  // selects random string number between 2 and 10
+
+        else 
+            // problem set 5 using spell array
+            problemSelect = spell[Math.floor(Math.random() * spell.length)];  // selects random word to spell
     };
 
     problemID = document.getElementById(problemSelect);  // alpha char matches html/css ID
@@ -44,9 +50,9 @@ function loadProblem() {
 
     // get answer
     if ( totalCorrect < 40  || totalCorrect >= 60 )
-        // for problem sets 1, 2, and 4 the answer is problemSelect
+        // for problem sets 1, 2, 4 and 5 the answer is problemSelect
         answer = problemSelect;  
-    else
+    else if ( totalCorrect >= 40 && totalCorrect < 60 )
         // for problem set 3 the answer is the second char in problemSelect (ex: "L4" - 4 is the answer)
         answer = problemSelect[1];  
 };
@@ -64,8 +70,15 @@ TEXTBOX.addEventListener("keydown", function (event) {
 
 // validates answer and initiates transition functions
 function checkAnswer(text, answer) {
+
+    // apply case insensitivty
+    if ( totalCorrect < 20 )
+        text = text.toUpperCase();
+    else if ( totalCorrect < 40 )
+        text = text.toLowerCase();
+
     if ( text === answer ) {
-        totalCorrect += 1;
+        totalCorrect++;
         transition();
         setTimeout(loadProblem, 500);  // 500ms must match setTimeout() in transition() function
 
@@ -122,10 +135,15 @@ function footboxGreen() {
         let footBox = document.getElementById("box" + set3);
         footBox.classList.add("footBoxCorrect");
 
-    } else {
+    } else if ( totalCorrect < 80 ) {
         // Tally for problem set 4
         set4 += 1;
         let footBox = document.getElementById("box" + set4);
+        footBox.classList.add("footBoxCorrect");
+    } else {
+        // Tally for problem set 5
+        set5 += 1;
+        let footBox = document.getElementById("box" + set5);
         footBox.classList.add("footBoxCorrect");
     }
 };
